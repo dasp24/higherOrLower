745361a9c2af12fcaf5fda2b5b575a8e36fea382
Dan Parkes <daniel.parkes901@gmail.com>
2020-06-20 11:41:41 +0100

    initial commit

---
+++ src/app/components/random-number/random-number.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random-number',
+  templateUrl: './random-number.component.html',
+  styleUrls: ['./random-number.component.scss']
+})
+export class RandomNumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
